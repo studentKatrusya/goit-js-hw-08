07273b619f7cf5cf7a8c09b54331234cdc4a9c3d
Katrusya Panchenko <katrusya.pan4enko@gmail.com>
2021-11-15 11:49:17 +0200

    добавляет решение первой задачи

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+const galleryContainer =document.querySelector('.gallery')
+const galleryItemMarkup = creategalleryItemMarkup(galleryItems)
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemMarkup);
+//создание макета разметки галереи
+function creategalleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return ` <div class = "gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+    
+      alt="${description}"
+    />
+  </a>
+</div>`;
+            
+    }).join('');
+ 
+}
+// добавление свойств
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
